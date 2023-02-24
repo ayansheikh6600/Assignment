81afbef79ff470f5aee26d5213d8a4f66b684b4b
ayansheikh6600 <ayansheikh6600@email.com>
2023-02-24 08:22:14 +0500

    Hellow

---
+++ app.js
@@ -0,0 +1,92 @@
+//Chapter Array
+//Question#1
+// var ayan =[]
+
+//Queation#2
+// var ayan =[
+//     {}
+// ]
+
+//Question#3
+// var ayan =["Ayan"]
+
+// Queation#4
+// var ayan =[1]
+
+//Question#5
+// var ayan =[true,false]
+
+//Question#6
+// var ayan =["Ayan" , 1 , false , true ]
+
+//Question#7
+// var education = ["SSC", "HSC" , "BSC" , "BS" , "BCOM" , "MS" , "M.Phil." , "PhD"]
+// document.write("<h1>Qualification</h1></br>");
+// document.write("1)."+education[0]+"<br>")
+// document.write("2)."+education[1]+"<br>")
+// document.write("3)."+education[2]+"<br>")
+// document.write("4)."+education[3]+"<br>")
+// document.write("5)."+education[4]+"<br>")
+// document.write("6)."+education[5]+"<br>")
+// document.write("7)."+education[6]+"<br>")
+// document.write("8)."+education[7]+"<br>")
+
+//Question#8
+// var studentName =["Michael" , "John" , "Tony"]
+// var studentMarks =[320 , 230 , 480]
+// var totalMark = 500;
+
+// var michael = (studentMarks[0]/totalMark)*100
+// var john = (studentMarks[1]/totalMark)*100
+// var tony = (studentMarks[2]/totalMark)*100
+
+// document.write("Score Of" + " " +studentName[0]+ " " + "is" + " " + studentMarks[0]+ " " + "Percentage:" +michael+"%" +"</br>");
+// document.write("Score Of" + " " +studentName[1]+ " " + "is" + " " + studentMarks[1]+ " " + "Percentage:" +john+"%" +"</br>");
+// document.write("Score Of" + " " +studentName[2]+ " " + "is" + " " + studentMarks[2]+ " " + "Percentage:" +tony+"%" +"</br>");
+
+//Question#9
+//part A
+// var colorName = ["Red", "Green"]
+// document.write(colorName +"</br>")
+// var a = prompt("Enter Color Name")
+// colorName.unshift(a)
+// document.write(colorName)
+
+//Part B
+// var colorName = ["Red", "Green"]
+// document.write(colorName +"</br>")
+// var a = prompt("Enter Color Name")
+// colorName.push(a)
+// document.write(colorName +"</br>")
+
+//Part C
+// var colorName = ["Red", "Green"]
+// document.write(colorName +"</br>")
+// var a = prompt("Enter Color Name")
+// var b = prompt("ENter Another Color Name")
+// colorName.unshift(a,b)
+// document.write(colorName +"</br>")
+
+// //Part D
+// colorName.shift()
+// document.write(colorName +"</br>")
+
+//PArt E
+// colorName.pop()
+// document.write(colorName +"</br>")
+
+// //Part F
+// var indexNum = +prompt("Enetr Index Num")
+// var clole =prompt("Enter Color Name")
+// colorName[indexNum] = clole
+// console.log(colorName)
+
+//Question#11
+// var citeis = ["Karachi" , "Lahor" , "Islamabad" , "Quetta" ,"Peshawar"]
+// var selectedCities = citeis.splice(0,3)
+// document.write(selectedCities)
+
+//Question#12
+// var arr = ["This"," is"," my"," cat"];
+// document.write(arr.join([]))
+
